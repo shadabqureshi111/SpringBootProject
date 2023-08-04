fb5e106d0b61615eb1e3e1231795ac5a44378159
shadabqureshi111 <82750340+shadabqureshi111@users.noreply.github.com>
2023-08-04 13:15:11 +0530

    Delete UserSubscription directory

--- UserSubscription/src/main/java/com/example/repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.model.Answer;
-
-public interface AnswerRepository extends JpaRepository<Answer,Long> {
-
-}
